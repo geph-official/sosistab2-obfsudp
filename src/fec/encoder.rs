463ae0a708522e859e81cbeb11504d716a228fa3
nullchinchilla <nullchinchilla@pm.me>
2023-11-06 18:21:40 -0500

    Update loss threshold in FecEncoder from 0.005 to 0.03 for improved error handling

--- src/fec/encoder.rs
+++ src/fec/encoder.rs
@@ -35,7 +35,7 @@ impl FecEncoder {
     pub async fn wait_parity(&mut self, loss: f64) -> Vec<ObfsUdpFrame> {
         self.timer.wait().await;
         self.timer.reset();
-        if loss < 0.005 {
+        if loss < 0.03 {
             self.unfecked.clear();
             smol::future::pending().await
         }
